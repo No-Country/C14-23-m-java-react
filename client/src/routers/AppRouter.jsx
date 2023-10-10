0e5d5138e9b760d868ab21842f493bd3482b5df9
AgustinGonzalez <agustin_.gonzalez_@hotmail.com>
2023-10-10 12:07:25 -0300

    fix

--- client/src/routers/AppRouter.jsx
+++ client/src/routers/AppRouter.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import StatisticsPage from "../pages/StatisticsPage";
@@ -8,21 +7,21 @@ import NotFoundPage from "../pages/NotFoundPage";
 import Layout from "../modules/Layout";
 
 function AppRouter() {
-  return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          <Route exact path="/statistics" element={<StatisticsPage />} />
-          <Route exact path="/user" element={<UserPage />} />
-          <Route exact path="/financialHistory" element={<HistoryPage />} />
-          {/* <Route exact path='/logOut' element = {<LogoutPage/>} /> */}
+	return (
+		<BrowserRouter>
+			<Layout>
+				<Routes>
+					<Route exact path="/" element={<HomePage />} />
+					<Route exact path="/statistics" element={<StatisticsPage />} />
+					<Route exact path="/user" element={<UserPage />} />
+					<Route exact path="/financialHistory" element={<HistoryPage />} />
+					{/* <Route exact path='/logOut' element = {<LogoutPage/>} /> */}
 
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
-  );
+					<Route path="*" element={<NotFoundPage />} />
+				</Routes>
+			</Layout>
+		</BrowserRouter>
+	);
 }
 
 export default AppRouter;
