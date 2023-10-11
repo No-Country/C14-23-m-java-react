0e64ae2c8eeb47e207cd48e6cbda623bc2c14bce
ChristopherPeralta <cperaltapenaloza@gmail.com>
2023-10-11 12:14:11 -0500

    repositoryIncome

--- finanzas/src/main/java/com/nocountry/finanzas/repositories/IncomeRepository.java
+++ finanzas/src/main/java/com/nocountry/finanzas/repositories/IncomeRepository.java
@@ -3,5 +3,5 @@ package com.nocountry.finanzas.repositories;
 import com.nocountry.finanzas.entities.Income;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IcomeRepository extends JpaRepository<Income, Long> {
+public interface IncomeRepository extends JpaRepository<Income,Long> {
 }
