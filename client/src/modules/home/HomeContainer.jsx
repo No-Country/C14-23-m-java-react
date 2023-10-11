9d7128180a6c6efef36d80f27134c98ed0a89c84
hernan-varela <hernanvarela45@gmail.comm>
2023-10-11 15:41:35 -0300

    Se creo el componente que muestra el historial de las ultimas 10 transacciones

---
+++ client/src/modules/home/HomeContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RecentActivity from './components/RecentActivity';
+
+function HomeContainer(props) {
+    return (
+        <>
+            <RecentActivity />
+        </>
+    );
+}
+
+export default HomeContainer;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import HomeContainer from '../modules/home/HomeContainer';
+
 
 function HomePage(props) {
     return (
         <div>
-            <h1>Home Page</h1>
+            <HomeContainer />
         </div>
     );
 }
