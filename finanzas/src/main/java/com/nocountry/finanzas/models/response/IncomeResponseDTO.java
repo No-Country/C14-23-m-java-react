040f2683419cee58b9163c2ec0c5405172642d1b
ChristopherPeralta <cperaltapenaloza@gmail.com>
2023-10-11 10:41:26 -0500

    modelRepository

---
+++ finanzas/src/main/java/com/nocountry/finanzas/models/response/IncomeResponseDTO.java
@@ -0,0 +1,9 @@
+package com.nocountry.finanzas.models.response;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class IncomeResponseDTO {
+
+    public IncomeResponseDTO
+}
