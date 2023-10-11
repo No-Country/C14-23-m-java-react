7594e8ffcd48071539c96c7108d0011c2421cd4e
ChristopherPeralta <cperaltapenaloza@gmail.com>
2023-10-11 09:40:17 -0500

    Entity

---
+++ finanzas/src/main/java/com/nocountry/finanzas/entities/Income.java
@@ -0,0 +1,37 @@
+package com.nocountry.finanzas.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@Table(name = "income")
+public class Income {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="amount", nullable = false)
+    private Double amount;
+
+    @Column(name="date", nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private LocalDate date;
+
+    @Column(name="description", nullable = false)
+    @Size(min = 0, max = 255, message = "La descripción no debe superar los 255 caracteres.")
+    private String description;
+
+
+}
