768056b0f43490403b3490657e7b43a1569e1d2f
AgustinGonzalez <agustin_.gonzalez_@hotmail.com>
2023-10-17 20:28:44 -0300

    fix the sprint 1 errors and added additional styles to the navbar.

--- client/src/modules/Login/LoginContainer.jsx
+++ client/src/modules/Login/LoginContainer.jsx
@@ -1,106 +1,124 @@
-import { Box, Button, Container, Grid, TextField, ThemeProvider, Typography, createTheme } from "@mui/material";
-import { useForm } from "react-hook-form";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  TextField,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const theme = createTheme({
-	palette: {
-		primary: {
-			main: "#00796B",
-		},
-	},
+  palette: {
+    primary: {
+      main: '#00796B',
+    },
+  },
 });
 
 const LoginContainer = () => {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-	const onSubmit = handleSubmit((data) => console.log(data));
+  const onSubmit = handleSubmit((data) => console.log(data));
 
-	return (
-		<ThemeProvider theme={theme}>
-			<Container component="main" maxWidth="xs">
-				<Box
-					sx={{
-						marginTop: 8,
-						display: "flex",
-						flexDirection: "column",
-						alignItems: "center",
-					}}>
-					<Typography component="h1" variant="h5">
-						Iniciar sesión
-					</Typography>
-				</Box>
-				<Box component="form" sx={{ mt: 2 }} onSubmit={onSubmit}>
-					<Grid container spacing={3}>
-						<Grid item xs={12}>
-							<TextField
-								fullWidth
-								type="email"
-								id={"email"}
-								label={"Email"}
-								name={"Email"}
-								{...register("email", {
-									required: "Email is required",
-									pattern: {
-										value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-										message: "Email is not valid",
-									},
-								})}
-								error={errors.email ? true : false}
-								helperText={errors.email?.message}
-							/>
-						</Grid>
-						<Grid item xs={12}>
-							<TextField
-								fullWidth
-								type="password"
-								id={"password"}
-								label={"Contraseña"}
-								name={"password"}
-								{...register("password", {
-									required: "Password is required",
-									minLength: {
-										value: 8,
-										message: "Password must be at least 8 characters long",
-									},
-									maxLength: {
-										value: 20,
-										message: "Password must be at most 20 characters long",
-									},
-								})}
-								error={errors.password ? true : false}
-								helperText={errors.password?.message}
-							/>
-						</Grid>
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component='main' maxWidth='xs'>
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component='h1' variant='h5'>
+            Iniciar sesión
+          </Typography>
+        </Box>
+        <Box component='form' sx={{ mt: 2 }} onSubmit={onSubmit}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                type='email'
+                id={'email'}
+                label={'Email'}
+                name={'Email'}
+                autoComplete={'email'}
+                {...register('email', {
+                  required: 'Email is required',
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: 'Email is not valid',
+                  },
+                })}
+                error={errors.email ? true : false}
+                helperText={errors.email?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                type='password'
+                id={'password'}
+                label={'Contraseña'}
+                name={'password'}
+                autoComplete={'current-password'}
+                {...register('password', {
+                  required: 'Password is required',
+                  minLength: {
+                    value: 8,
+                    message: 'Password must be at least 8 characters long',
+                  },
+                  maxLength: {
+                    value: 20,
+                    message: 'Password must be at most 20 characters long',
+                  },
+                })}
+                error={errors.password ? true : false}
+                helperText={errors.password?.message}
+              />
+            </Grid>
 
-						<Grid item xs={12}>
-							<Button
-								type="submit"
-								fullWidth
-								variant="contained"
-								sx={{ mb: 2, bgcolor: "#00796B", "&:hover": { bgcolor: "#006B5B" } }}>
-								Iniciar sesión
-							</Button>
-						</Grid>
-					</Grid>
-					<Grid container spacing={2}>
-						<Grid item xs={12} sm={6}>
-							<Typography href="#" component="a" variant="body2">
-								Olvidaste tu contraseña?
-							</Typography>
-						</Grid>
-						<Grid item xs={12} sm={6}>
-							<Typography href="#" component="a" variant="body2">
-								No tienes cuenta? Regístrate
-							</Typography>
-						</Grid>
-					</Grid>
-				</Box>
-			</Container>
-		</ThemeProvider>
-	);
+            <Grid item xs={12}>
+              <Button
+                type='submit'
+                fullWidth
+                variant='contained'
+                sx={{
+                  mb: 2,
+                  bgcolor: '#00796B',
+                  '&:hover': { bgcolor: '#006B5B' },
+                }}
+              >
+                Iniciar sesión
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <Typography href='#' component='a' variant='body2'>
+                Olvidaste tu contraseña?
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Typography to={'/register'} component={Link} variant='body2'>
+                No tienes cuenta? Regístrate
+              </Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default LoginContainer;
--- client/src/modules/navbar/Navbar.jsx
+++ client/src/modules/navbar/Navbar.jsx
@@ -10,17 +10,14 @@ import {
   Typography,
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import HomeIcon from '@mui/icons-material/Home';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import ShowChartIcon from '@mui/icons-material/ShowChart';
+import { NavLink } from 'react-router-dom';
 
 const navLinks = [
   { title: 'Landing', path: '/' },
-  { title: 'Inicio', path: '/home', icon: <HomeIcon /> },
-  { title: 'Estadísticas', path: '/statistics', icon: <BarChartIcon /> },
-  { title: 'Usuario', path: '/user', icon: <AccountCircleIcon /> },
-  { title: 'Historial', path: '/financialHistory', icon: <ShowChartIcon /> },
+  { title: 'Inicio', path: '/home' },
+  { title: 'Estadísticas', path: '/statistics' },
+  { title: 'Usuario', path: '/user' },
+  { title: 'Historial', path: '/financialHistory' },
   { title: 'iniciar sesión', path: '/login' },
   { title: 'registrarse', path: '/register' },
 ];
@@ -31,7 +28,7 @@ function NavBar() {
   return (
     <>
       <AppBar position='static'>
-        <Toolbar sx={{ bgcolor: '#8c8c8c' }}>
+        <Toolbar sx={{ bgcolor: '#00796B' }}>
           <IconButton
             color='inherit'
             size='large'
@@ -44,20 +41,25 @@ function NavBar() {
           </IconButton>
 
           <Box sx={{ display: { xs: 'none', md: 'block' } }}>
-            {' '}
             {/**para lograr responsive que se vea un menu o el otro */}
             {navLinks.map((item) => (
               <Button
-                color='inherit'
                 key={item.title}
-                component='a'
-                href={item.path}
+                component={NavLink}
+                to={item.path}
+                variant='contained'
+                sx={{ mx: '5px' }}
+                style={({ isActive }) => ({
+                  backgroundColor: isActive ? '#00796B' : 'transparent',
+                  boxShadow: isActive ? 'none' : 'none',
+                  border: isActive ? '1px solid white' : '1px solid #00796B',
+                })}
               >
                 {item.title}
               </Button>
             ))}
             <Button color='inherit' component='a' href='#cerrar sesion'>
-              Cerrar sesion
+              Cerrar sesión
             </Button>
           </Box>
         </Toolbar>
