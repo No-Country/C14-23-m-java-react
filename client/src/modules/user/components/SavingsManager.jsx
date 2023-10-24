a95997ce702406b5f01401879c1df34ddda0176a
hernan-varela <hernanvarela45@gmail.comm>
2023-10-24 16:08:28 -0300

    Se agregaron validaciones y se consume la informacion del usuario desde  el context

--- client/src/modules/user/components/SavingsManager.jsx
+++ client/src/modules/user/components/SavingsManager.jsx
@@ -1,32 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
+import Alert from '@mui/material/Alert';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import AddIcon from '@mui/icons-material/Add';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import Box from '@mui/material/Box';
+import { Collapse } from '@mui/material';
+import {useUser} from '../../../context/UserContext'
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 
 function SavingsManager(props) {
   const [savings, setSavings] = useState(0);
   const [inputValue, setInputValue] = useState('');
+  const [inputAlert, setInputAlert] = useState(false); // Estado para mostrar alerta si el input excede el total del saldo del usuario
+  const [amountSavingAlert, setAmountSavingAlert] = useState(false); // Estado para mostrar alerta si el inputes menor a el total del saldo del usuario pero supera el valor si es sumado
+  const [savedSavings, setSavedSavings] = useState(false) // Estado para mostrar un alerta si se guardo con exito un ahorro
+  const [infoUser, setInfoUser] = useState(null) // estado que guarda toda la informnacion del usuario
+  const [restMoney, setRestMoney] = useState(null)
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
+  
+  const{getDataUser} = useUser() //traigo el contexto user
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await getDataUser(1);
+        setInfoUser(res)
+        console.log(infoUser.totalIncome)
+        setRestMoney(infoUser.totalIncome?.toFixed(2))
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
+    // Verificar si se debe mostrar la alerta
+    const inputValueAsNumber = parseFloat(event.target.value);
+    if (inputValueAsNumber > infoUser.totalIncome) {
+      setInputAlert(true);
+    } else {
+      setInputAlert(false);
+    }
   };
 
   const handleAddClick = () => {
     const inputValueAsNumber = parseFloat(inputValue);
     if (!isNaN(inputValueAsNumber)) {
-      setSavings(savings + inputValueAsNumber);
-      setInputValue('');
+      if (inputValueAsNumber <= restMoney) {
+        setSavings(savings + inputValueAsNumber);
+        setSavedSavings(true);
+        setInputValue('');
+        setRestMoney(restMoney - inputValueAsNumber.toFixed(2));
+        // Restaurar la alerta después de 3 segundos
+        setTimeout(() => {
+          setSavedSavings(false);
+        }, 3000);
+      } else {
+        setAmountSavingAlert(true);
+      }
     }
   };
+  
 
   const handleResetClick = () => {
     setSavings(0);
@@ -34,7 +77,7 @@ function SavingsManager(props) {
   };
 
   return (
-    <Box display="flex" flexDirection={isSmallScreen ? 'column' : 'row'}>
+    <Box display='flex' flexDirection={isSmallScreen ? 'column' : 'row'}>
       <Paper
         sx={{
           display: 'flex',
@@ -45,7 +88,7 @@ function SavingsManager(props) {
           margin: '2rem',
         }}
       >
-        <Typography variant="h4">Gestiona tus ahorros</Typography>
+        <Typography variant='h4'>Gestiona tus ahorros</Typography>
         <Box
           sx={{
             display: 'flex',
@@ -68,7 +111,7 @@ function SavingsManager(props) {
           >
             <TextField
               value={inputValue}
-              type="number"
+              type='number'
               onChange={handleInputChange}
               InputProps={{
                 inputProps: {
@@ -76,12 +119,44 @@ function SavingsManager(props) {
                 },
               }}
             />
+
+             {/* alertas para el formulario */}
+            {inputAlert && (
+              <Alert
+                severity='error'
+                
+              >
+                El monto no puede ser mayor que 20,000
+              </Alert>
+            )}
+
+            <Collapse in={amountSavingAlert}>
+              <Alert
+                severity='error'
+                onClose={() => setAmountSavingAlert(false)}
+              >
+                el monto que quieres ingresar mas lo que ya tenias guardado
+                superan tu saldo
+              </Alert>
+            </Collapse>
+
+            <Collapse in={savedSavings}>
+              <Alert
+                severity='success'
+                onClose={() => setSavedSavings(false)}
+              >
+                Se ha establecido tu meta de ahorro mensual
+              </Alert>
+            </Collapse>
+
+             {/* fin  alertas para el formulario */}
+
             <Button
-              variant="contained"
-              color="primary"
+              variant='contained'
+              color='primary'
               startIcon={<AddIcon />}
               sx={{
-                backgroundColor : '#00796B',
+                backgroundColor: '#00796B',
                 '&:hover': {
                   backgroundColor: 'white',
                   border: '1px solid #00796B',
@@ -94,8 +169,8 @@ function SavingsManager(props) {
               Agregar
             </Button>
             <Button
-              variant="contained"
-              color="error"
+              variant='contained'
+              color='error'
               startIcon={<RefreshIcon />}
               sx={{
                 '&:hover': {
@@ -120,11 +195,21 @@ function SavingsManager(props) {
           padding: '1rem',
           width: isSmallScreen ? '90%' : '45%',
           margin: '2rem',
-          justifyContent:'center'
+          justifyContent: 'center',
         }}
       >
-        <Typography variant="h5">Este mes has decidido ahorrar:</Typography>
-        <Typography variant="h4">{`$${savings.toFixed(2)}`}</Typography>
+        <AccountBalanceIcon sx={{ fontSize: 64, color: '#00796B' }} />
+        <Typography variant="h5" sx={{ marginTop: '1rem' }}>
+          Este mes has decidido ahorrar:
+        </Typography>
+        <Typography variant="h4">
+          <span style={{ color: '#00796B' }}>
+            {`$${savings.toFixed(2)}`}
+          </span>
+        </Typography>
+        <Typography variant="h6" sx={{ marginTop: '2rem' }}>
+          Saldo disponible: <span style={{ color: '#00796B' }}>{`$${restMoney}`}</span>
+        </Typography>
       </Paper>
     </Box>
   );
