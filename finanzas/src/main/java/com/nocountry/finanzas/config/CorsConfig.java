17029cb64e3070b47a4e3e02e9ef433c12c101c2
Melisa Yunis <yunismelisa@gmail.com>
2023-10-28 22:23:42 -0300

    *

--- finanzas/src/main/java/com/nocountry/finanzas/config/CorsConfig.java
+++ finanzas/src/main/java/com/nocountry/finanzas/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
-                .allowedHeaders("Authorization")
+                .allowedHeaders("Authorization", "*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowCredentials(true);
     }
